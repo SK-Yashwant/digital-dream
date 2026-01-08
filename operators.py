f8b51f504f6c598c67160f0889e2ac1622948d96
SK-Yashwant <karabhariyashwant7@gmail.com>
2026-01-08 19:59:51 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,60 @@
+price=1500
+gst=0.18*price
+total_bill=price+gst
+print("total bill amount :",total_bill)
+
+total_stu=100
+groups=3
+reminder=total_stu%groups
+print(reminder)
+
+age =18
+print(age)
+if age >= 18 :
+    print("eligible")
+else :
+    print("not eligible")
+
+emp_01 = 25000
+emp_02 = 30000
+print(emp_01)
+print(emp_02)
+
+if emp_02 > emp_02:
+    print("emp_01 earn's more")
+else:
+    print("emp_02 earn's more ")
+
+    user = "yashwant"
+    print(user)
+
+    suspend_user = input("enter the user name ")
+    if suspend_user != user :
+        print("suspend_user accessed")
+    else:
+        print("not a suspend_user")
+
+wallet= 2000
+wallet-= 750
+print("remanning wallet balence:",wallet)
+
+
+attendance = ["Rahul", "Anita", "Suresh"]
+
+print("Anita" in attendance)
+
+
+inventory = ["Laptop", "Mobile", "Headphones"]
+
+if "Mobile" in inventory:
+    print("Product Available")
+else:
+    print("Product Not Available")
+
+
+a = 10
+b = 10
+c = 20
+
+print(a is b)
+print(a is not c)
