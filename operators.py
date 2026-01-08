79df08a0d5ae269cc2ae6eb0517452032de3a18c
SK-Yashwant <karabhariyashwant7@gmail.com>
2026-01-08 19:37:00 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,18 @@
+total_bill = 10000
+gst = 18/100
+amount = total_bill * gst
+print(amount)
+
+
+total_stu = 100
+group = 3
+
+reminder = total_stu % group
+print(reminder)
+
+consumption = 10
+exponent = 2
+
+power = consumption^exponent
+print(power)
+
